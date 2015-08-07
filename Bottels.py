bcf1aee6ad0145cb10cf4869f561015dd9ef1c24
codehArt <hardikp40@gmail.com>
2015-08-07 18:00:50 +0530

    bhai thai gyu ho

--- Bottels.py
+++ Bottels.py
@@ -1,21 +1,24 @@
-N = int(raw_input("Enter N:"))
+N = int(raw_input())
 S = []
 
 for i in range(0, N):
-	S.append(int(raw_input("\nEnter S:")))
+	S.append(int(raw_input()))
 
-X = int(raw_input("Enter X:"))
+X = int(raw_input())
 
-print X
+result = False
 
+#print X
 for i in range(0,N):
 
 	for j in range(0,(N-1)):
 		
 		for k in range(0,(N-2)):
 			if S[i]+S[j]+S[k]==X and i != k and i != j and j != k:
-				print "true"
+				result = True
+			
+print result
 
 		
 		
-		# if i + S[] +
+		
