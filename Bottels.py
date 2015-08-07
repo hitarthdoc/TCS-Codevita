72cfb475fc89583ee3621f7feb8a0bc99da077e0
hitarthdoc <hitarthdoctor1994@gmail.com>
2015-08-07 17:25:35 +0530

    Added Bottles/py

---
+++ Bottels.py
@@ -0,0 +1,15 @@
+N = int(raw_input("Enter N:"))
+S = []
+
+for i in range(0, N):
+	S.append(int(raw_input("\nEnter S:")))
+
+X = int(raw_input("Enter X:"))
+
+print X
+
+for i in S:
+
+	for j,k in range(0, N), range(0, N):
+		pass
+		# if i + S[] +
