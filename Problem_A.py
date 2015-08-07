d8c2d56773509995b5c056de2d86269ee881685d
hitarthdoc <hitarthdoctor1994@gmail.com>
2015-08-07 20:49:03 +0530

    Infinite loop?

--- Problem_A.py
+++ Problem_A.py
@@ -1,3 +1,30 @@
-N= int (raw_input())
+N= int (input())
+for i in range(0, N):
+    
+    F, B, T, FD, BD = map(int, input().split())
 
-F, B, T, FD, BD = abs(int(raw_input()))
+    current = 0
+    time = 0
+    
+    if F == B and F > FD:
+        print("No Ditch")
+
+    elif F > B:
+        while current != FD:
+            if (current + F) < FD:
+                current += (F - B)
+                time = (F + B) * T
+            else:
+                current = current + (F - (current - FD))
+                time = (F - (current - FD)) * T
+        print(time, "F")
+    
+    else:
+        while current != BD:
+            if B > (current - BD):
+                current += (F - B)
+                time = (F + B) * T
+            else:
+                current = current + (F - B + (current - BD))
+                time = (F - B + (current - FD)) * T
+        print(time, "B")
