826b7c2b581aec1789457a6fcba02cf89e6e72b9
hitarthdoc <hitarthdoctor1994@gmail.com>
2015-08-07 20:00:32 +0530

    Remove Presentaion Error

--- Bottels.py
+++ Bottels.py
@@ -1,25 +1,18 @@
 N = int(input())
-if N >= 3:
+A = []
 
-    A = []
-    flag = False
-
-    for i in range(0, N):
-        A.append(int(input()))
-        if A[i] > 0:
-            flag = True
+for i in range(0, N):
+    A.append(int(input()))
     
-    if flag==False:
-        X = int(input())
+X = int(input())
 
-        if X >0:
-            result = False
+result = False
 
-            for i in range(0,N):
+for i in range(0,N):
 
-                for j in range(0,(N-1)):
-                    
-                    for k in range(0,(N-2)):
-                        if A[i]+A[j]+A[k]==X and i != k and i != j and j != k:
-                            result = True
-            print(result)
+    for j in range(0,(N-1)):
+        
+        for k in range(0,(N-2)):
+            if (A[i]+A[j]+A[k])==X and i != k and i != j and j != k:
+                result = True
+print(result)
