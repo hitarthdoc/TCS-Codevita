68f4b3c42e29b8d05656abe6b2a1d496b9407207
codehArt <hardikp40@gmail.com>
2015-08-07 18:45:28 +0530

    bhai thai gyu ho

--- Bottels.py
+++ Bottels.py
@@ -1,24 +1,21 @@
 N = int(raw_input())
-S = []
+if N >= 3:
 
-for i in range(0, N):
-	S.append(int(raw_input()))
+    S = []
 
-X = int(raw_input())
+    for i in range(0, N):
+        S.append(int(raw_input()))
 
-result = False
+    X = int(raw_input())
 
-#print X
-for i in range(0,N):
+    result = False
 
-	for j in range(0,(N-1)):
-		
-		for k in range(0,(N-2)):
-			if S[i]+S[j]+S[k]==X and i != k and i != j and j != k:
-				result = True
-			
-print result
+    for i in range(0,N):
 
-		
-		
-		
+        for j in range(0,(N-1)):
+            
+            for k in range(0,(N-2)):
+                if S[i]+S[j]+S[k]==X and i != k and i != j and j != k:
+                    result = True
+                
+    print result
