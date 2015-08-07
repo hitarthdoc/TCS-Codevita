f3747a15a928a1d41932b5c900f0a39459d2de05
hitarthdoc <hitarthdoctor1994@gmail.com>
2015-08-07 19:40:25 +0530

    Done Deal

--- Bottels.py
+++ Bottels.py
@@ -1,26 +1,25 @@
-N = int(raw_input())
+N = int(input())
 if N >= 3:
 
     A = []
+    flag = False
 
     for i in range(0, N):
-        A.append(int(raw_input()))
-        if A[i] <0:
-            exit()
+        A.append(int(input()))
+        if A[i] > 0:
+            flag = True
+    
+    if flag==False:
+        X = int(input())
 
-    X = int(raw_input())
+        if X >0:
+            result = False
 
-    if X <0:
-        exit()
+            for i in range(0,N):
 
-    result = False
-
-    for i in range(0,N):
-
-        for j in range(0,(N-1)):
-            
-            for k in range(0,(N-2)):
-                if A[i]+A[j]+A[k]==X and i != k and i != j and j != k:
-                    result = True
-                
-    print result
+                for j in range(0,(N-1)):
+                    
+                    for k in range(0,(N-2)):
+                        if A[i]+A[j]+A[k]==X and i != k and i != j and j != k:
+                            result = True
+            print(result)
