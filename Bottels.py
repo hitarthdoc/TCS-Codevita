6445df9cd0823bb6184dd12c39bf6dbae902518b
codehArt <hardikp40@gmail.com>
2015-08-07 17:53:04 +0530

    bhai thai gyu ho

--- Bottels.py
+++ Bottels.py
@@ -8,8 +8,14 @@ X = int(raw_input("Enter X:"))
 
 print X
 
-for i in S:
+for i in range(0,N):
 
-	for j,k in range(0, N), range(0, N):
-		pass
+	for j in range(0,(N-1)):
+		
+		for k in range(0,(N-2)):
+			if S[i]+S[j]+S[k]==X and i != k and i != j and j != k:
+				print "true"
+
+		
+		
 		# if i + S[] +
