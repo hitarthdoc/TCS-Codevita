a22ad3eaa7328258a340694934d6c4b4690faf4d
hitarthdoc <hitarthdoctor@gmail.com>
2015-08-27 15:31:44 +0530

    did something

--- Round_2/Insect_War.py
+++ Round_2/Insect_War.py
@@ -1,4 +1,4 @@
- def insect(lst, t):
+def insect(lst, t):
     # print(t)
 
     if t != 0:
@@ -8,29 +8,30 @@
         lst.sort()
         # print( lst)
 
-        temp = list()
+        abc = []
 
         for i in range(0, len(lst) ):
 
             print("\nsjdhgf", i)
 
             if i == 0:
-                temp.append( lst[i] )
-                # print( "sdrjgh", temp)
+                abc.append( lst[i] )
+                # print( "sdrjgh", abc)
 
             else:
                 for j in range(0, i):
 
                     a = lst[i] + lst[j]
                     print(a)
-                    temp.append(a)
-                
-                temp.append( lst[i] )
+                    abc[ len(abc): ] = [a]
+                    
 
-                temp = list( set( lst ) )
-                temp.sort()
-        print(temp)
-        ans = insect(temp, t)
+                abc.append( lst[i] )
+
+                abc = list( set( lst ) )
+                abc.sort()
+        print(abc)
+        ans = insect(abc, t)
 
         return ans
 
@@ -41,8 +42,8 @@ n = int(input())
 l = [ [] for x in range(n) ]   
 for x in range(0,n):
     l[x] = list(map(int,input().split()))
-    if (0 < l[x][0] && l[x][0] < l[x][1] && l[x][1] <10000001):
-        break:
+    if (0 < l[x][0] and l[x][0] < l[x][1] and l[x][1] <10000001):
+        break
  
 
 a = insect( [2,5], 3 )
