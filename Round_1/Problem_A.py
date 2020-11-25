3daac2d0cf75b19ab2e1903d7fc3216e37a5aba0
Hitarth Doctor <hitarthdoctor1994@gmail.com>
2020-11-25 11:55:45 +0530

    Replaced loops with No LOOP calculations.

--- Round_1/Problem_A.py
+++ Round_1/Problem_A.py
@@ -1,36 +1,52 @@
-N= int (input())
+import math
+
+N = int (input())
 
 for i in range(0, N):
-    
-    F, B, T, FD, BD = map(int, input().split())
+	
+	F, B, T, FD, BD = map(int, input().split())
+	direction = 0;
+	if F > FD:
+		print( T * FD, "F")
+		continue
 
-    current = 0
-    time = 0
-    
-    if F == B and F < FD:
-        print("No Ditch")
+	elif F == B and F < FD:
+		print("No Ditch")
+		continue
 
-    elif F >= B:
-        while current != FD:
-            if (current + F) < FD:
-                current = current + (F - B)
-                time = time + ((F + B) * T)
-            else:
-                time = time - ((current - FD) * T)
-                current = FD
-        print(time, "F")
-        # ans[i] = str(time) + " F"
-    
-    else:
-        while current != BD:
-            if (current + (F - B)) > (-BD):
-                current = current + (F - B)
-                time = time + ((F + B) * T)
-            else:
-                time = time + ((F + B + (BD - (B - ( current + F)))) *T)
-                current = BD
-        print(time, "B")
-        # ans[i] = str(time) + " B"
+	elif (F > B):
+		direction = 1
+	else:
+		direction = -1
 
-# for i in range(0, (len(ans))):
-#     print(ans[i])
+	dist_per_turn = 0
+	turns = 0
+	total_distance_travelled = 0
+
+	if direction == 1:
+		dist_per_turn = F - B
+		turns = int( math.floor ( ( FD / dist_per_turn ) ) ) - 1
+		last_position = dist_per_turn * turns
+		delta_dist = FD - last_position
+		if (delta_dist > 0):
+			total_distance_travelled = ( (F + B) * turns ) + delta_dist
+		else:
+			total_distance_travelled = ( (F + B) * turns )
+
+		print (total_distance_travelled, dist_per_turn, turns, last_position, delta_dist)
+		print (total_distance_travelled * T, "F")
+	elif direction == -1:
+		dist_per_turn = B - F
+		turns = int( math.floor ( ( BD / dist_per_turn ) ) )
+		last_position = dist_per_turn * turns
+		delta_dist = BD - last_position
+		if (delta_dist > 0):
+			total_distance_travelled = ( (F + B) * turns ) + (2 * F) + delta_dist
+		else:
+			total_distance_travelled = ( (F + B) * turns )
+			
+		print (total_distance_travelled, dist_per_turn, turns, last_position, delta_dist)
+		print (total_distance_travelled * T, "B")
+
+	# else:
+	# 	continue
